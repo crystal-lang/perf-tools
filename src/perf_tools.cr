b7794b5be52c027e2ae1e928f2ca90daa618a87c
Quinton Miller <nicetas.c@gmail.com>
2023-09-11 23:43:44 +0800

    add shard.yml

---
+++ src/perf_tools.cr
@@ -0,0 +1,7 @@
+# An assortment of tools to track resources in Crystal applications.
+#
+# NOTE: The tools must be required separately; doing `require "perf_tools"` will
+# not enable any of the tools.
+module PerfTools
+  VERSION = "0.1.0"
+end
