84b52ef96e8bc7faa45f0187cb02326df7d26873
Julien Portalier <julien@portalier.com>
2024-05-24 15:22:17 +0200

    Fix: current fiber is now stored on Thread (not Scheduler) (#16)
    * Apply suggestions by @straight-shoota

--- src/perf_tools/fiber_trace.cr
+++ src/perf_tools/fiber_trace.cr
@@ -233,8 +233,13 @@ class Crystal::Scheduler
     while stack.last? == Pointer(Void).null
       stack.pop
     end
-    PerfTools::FiberTrace.yield_stack[@current] = stack
-
+    current_fiber = {% if Crystal::Scheduler.instance_vars.any? { |x| x.name == :thread.id } %}
+                      # crystal >= 1.13
+                      @thread.current_fiber
+                    {% else %}
+                      @current
+                    {% end %}
+    PerfTools::FiberTrace.yield_stack[current_fiber] = stack
     previous_def
   end
 end
