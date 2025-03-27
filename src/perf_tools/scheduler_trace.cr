8875c2ce003ec01e9895b00478fdc838a1ae1b9c
Julien Portalier <julien@portalier.com>
2025-03-27 21:18:02 +0100

    SchedulerTrace: use fiber yield stack when FiberTrace is defined

--- src/perf_tools/scheduler_trace.cr
+++ src/perf_tools/scheduler_trace.cr
@@ -3,8 +3,6 @@
 require "./common"
 
 class Fiber
-  property __yield_trace = Slice(Void*).new(Pointer(Void*).null, 0)
-
   def status : String
     if @alive
       if @context.@resumable == 1
@@ -18,29 +16,7 @@ class Fiber
   end
 end
 
-module Fiber::ExecutionContext::Scheduler
-  def swapcontext(fiber : Fiber)
-    # record the yield stack
-    size = PerfTools::SchedulerTrace::BACKTRACE_SKIP + PerfTools::SchedulerTrace::BACKTRACE_DEPTH
-    ptr = GC.malloc_atomic(sizeof(Void*) * size).as(Void**)
-    ptr.clear(size)
-    slice = Slice(Void*).new(ptr, size)
-    Exception::CallStack.unwind_to(slice)
-
-    # cleanup and save
-    start = PerfTools::SchedulerTrace::BACKTRACE_SKIP
-    stop = -1
-    while slice[stop].null?; stop -= 1; end
-    Fiber.current.__yield_trace = slice[start..stop]
-
-    previous_def
-  end
-end
-
 module PerfTools::SchedulerTrace
-  BACKTRACE_DEPTH = ENV.fetch("FIBERTRACE_STACK_DEPTH", "10").to_i
-  BACKTRACE_SKIP = ENV.fetch("FIBERTRACE_STACK_SKIP_YIELD", "0").to_i
-
   {% if flag?(:unix) %}
     # Installs a signal handler to call `.print_runtime_status` on demand.
     #
@@ -48,8 +24,10 @@ module PerfTools::SchedulerTrace
     # example `LibC::SIGRTMIN + 7`. You may also register multiple signals, one
     # with fiber detail and the another without for example.
     def self.on(signal : Int32 = Signal::USR1.value, details : Bool = false) : Nil
-      # make sure that debug info has been loaded
-      Exception::CallStack.load_debug_info if details
+      if details &&  Fiber.current.responds_to?(:__yield_stack)
+        # make sure that debug info has been loaded
+        Exception::CallStack.load_debug_info
+      end
 
       # not using Signal#trap so the signal will be handled directly instead
       # of through the event loop that may have to wait (or be blocked in
@@ -76,8 +54,10 @@ module PerfTools::SchedulerTrace
   # Starts a thread that will call `.print_runtime_status` at every *interval*
   # until the program terminates.
   def self.every(interval : Time::Span, details = false) : Nil
-    # make sure that debug info has been loaded
-    Exception::CallStack.load_debug_info if details
+    if details && Fiber.current.responds_to?(:__yield_stack)
+      # make sure that debug info has been loaded
+      Exception::CallStack.load_debug_info
+    end
 
     Thread.new("SCHEDTRACE") do
       loop do
@@ -182,8 +162,8 @@ module PerfTools::SchedulerTrace
   private def self.print_runtime_status(fiber : Fiber, details = false) : Nil
     Crystal::System.print_error("  Fiber %p name=%s status=%s\n", fiber.as(Void*), fiber.name, fiber.status)
 
-    if details && (fiber.status != "running")
-      fiber.__yield_trace.each do |ip|
+    if details && (fiber.status != "running") && fiber.responds_to?(:__yield_stack)
+      fiber.__yield_stack[PerfTools::FiberTrace::STACK_SKIP_SPAWN..].each do |ip|
         Crystal::System.print_error("    ")
         Exception::CallStack.__perftools_print_frame(ip)
       end
